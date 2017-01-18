694143e74d647c41ff6d8cffeb086710b29db039
Bryan Summers <bsummersgb@gmail.com>
2017-01-18 22:53:18 +0800

    implement jq each to retrieve list of results

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,36 +1,40 @@
 $( document ).ready(function() {
 
-  $.ajax({
-    url: 'https://wind-bow.gomix.me/twitch-api/streams/freecodecamp',
-    dataType: 'jsonp',
-    success: function(data) {
-      if (data.stream) { // i.e. if it's not null
-        console.log("This channel is currently streaming: ", data.stream.channel.status);
-      } else {
-        console.log("No live stream, but here's the channel info:");
-        $.ajax({
-          url: 'https://wind-bow.gomix.me/twitch-api/channels/freecodecamp',
-          dataType: 'jsonp',
-          success: function(channelData) {
-            console.log(channelData);
+  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-          }
-        });
+  $.each(channels, function(i, channelName){
+    $.ajax({
+      url: 'https://wind-bow.gomix.me/twitch-api/streams/' + channelName,
+      dataType: 'jsonp',
+      success: function(data) {
+        if (data.stream) { // i.e. if it's not null
+          console.log("This channel is currently streaming: ", data.stream.channel.url);
+        } else {
+          console.log("No live stream, but here's the channel info:");
+          $.ajax({
+            url: 'https://wind-bow.gomix.me/twitch-api/channels/' + channelName,
+            dataType: 'jsonp',
+            success: function(channelData) {
+              console.log(channelData.url);
+            }
+          }); // close inner $.ajax()
+        }
       }
-
-
-    }
-  });
-
-
-
-
-
-
-});
+    }); // close outer $.ajax()
+  }); // close $.each()
+}); // close .ready()
 
 
 /* deleted accounts
   - brunofin
   - comster404
 */
+
+//
+// $.ajax({
+//   url: 'https://wind-bow.gomix.me/twitch-api/channels/ESL_SC2',
+//   dataType: 'jsonp',
+//   success: function(data) {
+//     console.log(data);
+//   }
+// });
