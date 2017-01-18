dacce201e358d11f7ad250c91bf11cf0cea5e7d6
Bryan Summers <bsummersgb@gmail.com>
2017-01-18 13:38:55 +0800

    develop working call to the twitch api

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,36 @@
 $( document ).ready(function() {
 
+  $.ajax({
+    url: 'https://wind-bow.gomix.me/twitch-api/streams/freecodecamp',
+    dataType: 'jsonp',
+    success: function(data) {
+      if (data.stream) { // i.e. if it's not null
+        console.log("This channel is currently streaming: ", data.stream.channel.status);
+      } else {
+        console.log("No live stream, but here's the channel info:");
+        $.ajax({
+          url: 'https://wind-bow.gomix.me/twitch-api/channels/freecodecamp',
+          dataType: 'jsonp',
+          success: function(channelData) {
+            console.log(channelData);
+
+          }
+        });
+      }
+
+
+    }
+  });
+
+
+
+
 
 
 });
+
+
+/* deleted accounts
+  - brunofin
+  - comster404
+*/
